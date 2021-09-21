a33412a84e123878e2efa67645a803c9e1727e0a
JenyaFromOmsk <90650551+JenyaFromOmsk@users.noreply.github.com>
2021-09-21 19:19:02 +0600

    Task 2.1.3 done

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,19 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private final Animal animal;
+
+    public AnimalsCage(@Qualifier("Dog") Animal animal) {
+        this.animal = animal;
+    }
 
     @Autowired
-    private Animal animal;
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +21,9 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return this.timer;
     }
 }
